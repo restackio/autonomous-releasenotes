a324acc18124f97be618b8c299dcf0bbb2539067
Osman Hernandez <osman.sist@gmail.com>
2024-10-09 18:02:19 +0200

    create release function

---
+++ backend/src/functions/createRelease.ts
@@ -0,0 +1,39 @@
+import { githubClient } from '../utils/githubClient.js';
+
+export async function createRelease(
+  {
+    owner,
+    repo,
+    tagName,
+    releaseName,
+    releaseBody,
+    branch,
+  }: {
+    owner: string;
+    repo: string;
+    tagName: string;
+    releaseName: string;
+    releaseBody: string;
+    branch?: string;
+  }
+) {
+  try {
+    const { octokit } = await githubClient();
+    const { data } = await octokit.rest.repos.createRelease({
+      owner,
+      repo,
+      tag_name: tagName,
+      name: releaseName,
+      body: releaseBody,
+      draft: false,
+      prerelease: false,
+      generate_release_notes: true,
+      ...(branch && { target_commitish: branch }),
+    });
+    
+    return data;
+    
+  } catch (error) {
+    throw new Error(`Error while creating release: ${error}`);
+  }
+}
